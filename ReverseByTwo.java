25d133998ef3fb00cc755bf1d96f1319a6333b0a
Tushar Rajendra Patil <94185256+tusharpatil9611@users.noreply.github.com>
2024-07-18 12:32:47 +0530

    Update ReverseByTwo.java

--- ReverseByTwo.java
+++ ReverseByTwo.java
@@ -3,22 +3,20 @@ package ArrayAssign;
 public class ReverseByTwo {
 
 	public static void main(String[] args) {
-		int [] a={12,45,87,95,67,140};
-		int i=0;
-		int j=a.length;
-		while(j>i)
-		{
-			a[i]=a[i]+a[j];
-			a[j]=a[i]-a[j];
-			a[i]=a[i]-a[j];
-			i++;
-			j--;
-			
-		}
-		for (int k = 0; i < a.length; i++) 
-		{
-			System.out.print(a[k]+" ");
-		}
+		   int[] a = {12, 45, 87, 95, 67, 140};
+        int i = 0;
+        int j = a.length-1;
+        while (j > i) {
+            a[i] = a[i] + a[j];
+            a[j] = a[i] - a[j];
+            a[i] = a[i] - a[j];
+            i++;
+            j--;
+
+        }
+        for (int k = 0; k < a.length; k++) {
+            System.out.print(a[k] + " ");
+        }
 
 	}
 
